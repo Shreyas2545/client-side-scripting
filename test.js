1051cc5fdf3696d4639d42cd2d19a74951c584eb
Shreyas2545 <shreyashadawale25@gmail.com>
2026-01-22 15:48:50 +0530

    pushing files

--- test.js
+++ test.js
@@ -1,12 +1,32 @@
-var myApp = angular.module("myModule", []);
+var myApp = angular.module("eventModule", []);
 
-myApp.controller("myController", function ($scope) {
-    $scope.styleData = {
-        "color": "green",
-        "background-color": "white",
-        "font-family": "sans-serif",
-        "text-align": "center",
-        "font-size": "50px"
+myApp.controller("eventController", function ($scope) {
+
+    $scope.events = [
+        {
+            name: "Hackathon",
+            venue: "Computer Lab A",
+            time: "9:00 AM – 5:00 PM"
+        },
+        {
+            name: "Technical Workshop",
+            venue: "Seminar Hall 1",
+            time: "10:00 AM – 1:00 PM"
+        },
+        {
+            name: "Tech Talk",
+            venue: "Auditorium",
+            time: "2:00 PM – 4:00 PM"
+        },
+        {
+            name: "Coding Contest",
+            venue: "Computer Lab B",
+            time: "11:00 AM – 2:00 PM"
+        }
+    ];
+
+    $scope.submitForm = function () {
+        $scope.submitted = true;
     };
-    $scope.userName = " ";
+
 });
